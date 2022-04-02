199ebe6947f16dd42ff4247b3d763e7854e96c24
Richard_R <37909374+nomodfx@users.noreply.github.com>
2022-04-01 17:58:25 -0700

    Updated OpExp and added tokens to Parser

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -4,25 +4,32 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Parser {
-	private final List<Token> tokens;
-	
-	public Parser(final List<Token> tokens) {
-		this.tokens = tokens;
-	}
-	
-	public Token getToken(final int position) throws ParseException {
-		if(position >= 0 && position < tokens.size()) {
-			return tokens.get(position);
-		} else {
-			throw new ParseException("Invalid token position: " + position);
-		}
-	}
-	
-	public void assertTokenHereIs(final int position, final Token expected) throws ParseException{
-		final Token received = getToken(position);
-		if(!expected.equals(received)) {
-			throw new ParseException("expected: " + expected + "; received: " + received);
-		}
-	}
+    private final List<Token> tokens;
 
+    public Parser(final List<Token> tokens) {
+        this.tokens = tokens;
+    }
+    
+    public Token getToken(final int position) throws ParseException {
+        if (position >= 0 && position < tokens.size()) {
+            return tokens.get(position);
+        } else {
+            throw new ParseException("Invalid token position: " + position);
+        }
+    }
+    public void assertTokenHereIs(final int position, final Token expected) throws ParseException {
+        final Token received = getToken(position);
+        if (!expected.equals(received)) {
+            throw new ParseException("expected: " + expected + "; received: " + received);
+        }
+    }
+    
+    //ParseResult<Exp>, exp
+   
+    //ParserResult<Op>, additive_op
+    
+    //ParserResult<Op>, additive_exp
+   
+    //parse stmt
+    
 }
