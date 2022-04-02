76131ab71118c02a31b14a5dd5bccf37c92ef385
Richard_R <37909374+nomodfx@users.noreply.github.com>
2022-04-01 18:48:21 -0700

    Create ParserTest.java
    will add more tests

---
+++ src/test/java/parser/ParserTest.java
@@ -0,0 +1,20 @@
+package parser;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ParserTest {
+    @Test
+    public void testEqualsOpExp() {
+        // 1 + 1 == 1 + 1
+        final OpExp first = new OpExp(new IntegerExp(1),
+                                      new AddOp(),
+                                      new IntegerExp(1));
+        final OpExp second = new OpExp(new IntegerExp(1),
+                                      new AddOp(),
+                                      new IntegerExp(1));
+        assertEquals(first, second);
+    }
+}
