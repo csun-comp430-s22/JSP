49577c2d91a2fbc9d34e76fe25587391b5845c22
Richard_R <37909374+nomodfx@users.noreply.github.com>
2022-04-04 14:40:40 -0700

    fixed VariableExp

--- src/main/java/parser/VariableExp.java
+++ src/main/java/parser/VariableExp.java
@@ -1,22 +1,22 @@
 package parser;
 
 public class VariableExp implements Exp{
-	public final String name;
+	public final Var var;
 	
-	public VariableExp(final String name) {
-		this.name = name;
+	public VariableExp(final Var var) {
+		this.var = var;
 	}
 	
 	public boolean equals(final Object other) {
-		return(other instanceof VariableExp && name.equals(((VariableExp)other).name));
+		return(other instanceof VariableExp && var.equals(((VariableExp)other).var));
 	}
 	
 	public int hashCode() {
-		return name.hashCode();
+		return var.hashCode();
 	}
 	
 	public String toString() {
-		return "VariableExp(" + name + ")";
+		return "VariableExp(" + var + ")";
 	}
 
 }
