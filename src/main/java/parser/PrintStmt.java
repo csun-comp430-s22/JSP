6f8ee80e3ae170c001cd30a2a475bb8f48c37126
Richard_R <37909374+nomodfx@users.noreply.github.com>
2022-04-04 13:53:07 -0700

    added PrintStmt class

---
+++ src/main/java/parser/PrintStmt.java
@@ -0,0 +1,21 @@
+package parser;
+
+public class PrintStmt implements Stmt{
+	public final Exp exp;
+	
+	public PrintStmt(final Exp exp) {
+		this.exp = exp;
+	}
+	
+	public int hashCode() {
+		return exp.hashCode();
+	}
+	
+	public boolean equals(final Object other) {
+		return(other instanceof PrintStmt && exp.equals(((PrintStmt)other).exp));
+	}
+	
+	public String toString() {
+		return "Print" + exp.toString() + ")";
+	}
+}
