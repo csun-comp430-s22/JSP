c2accdaeea67af4d4dedec9dbdb283632fc7b79b
nikkoaustria <nikkoaustria6@gmail.com>
2022-05-05 20:35:16 -0700

    fixed StructNameType class

--- src/main/java/parser/StructNameType.java
+++ src/main/java/parser/StructNameType.java
@@ -1,15 +1,21 @@
 package parser;
 
 public class StructNameType implements Type {
-	public boolean equals(final Object types) {
-		return types instanceof StructNameType;
+	public final StructName name;
+	
+	public StructNameType(final StructName name) {
+		this.name = name;
+	}
+	
+	public boolean equals(final Object other) {
+		return(other instanceof StructNameType && name.equals(((StructNameType)other).name));
 	}
 	
 	public int hashCode() {
-		return 32;
+		return name.hashCode();
 	}
 	
 	public String toString() {
-		return "StuctNameType";
+		return "StructNameType(" + name.toString() + ")";
 	}
 }
